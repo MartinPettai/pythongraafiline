224dc8d1e8122aab54cb558b6c22fee206ab81e3
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-03-15 09:38:52 +0200

    Add files via upload

---
+++ üle3.gr2.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import random
+import turtle
+#martin pettai
+#it21
+#ül3gr2
+
+
+aken=Tk()
+aken.geometry("300x300+100+100")
+tekst=Label(aken, text="Nimi: Karin Eegried", foreground="red", background="black", font="Tahoma 16 bold italic").pack()
